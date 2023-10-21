d9d4fe391c194764d2dc56d2e209ac7a0254b4b3
Diogo Oliveira <djfreitasoliveira@gmail.com>
2023-10-21 19:21:29 -0400

    App.java

--- AULA-11/Atividade/Atividade - 06/FakeDB-Escola/src/App.java
+++ AULA-11/Atividade/Atividade - 06/FakeDB-Escola/src/App.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
+import com.uniderp.projetoescola.FakeDB.Turma;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        Turma turma = new Turma();
+        int opcao;
+        String menu = "\n\n1- Gerenciar Alunos\n2- Rendimento Escolar\n9- Sair\n\n";
+        do {
+            System.out.println(menu);
+            opcao = sc.nextInt();
+            switch (opcao) {
+                case 1:
+                    System.out.println("Opção 1");
+                    
+                    break;
+                case 2:
+                    System.out.println("Opção 2");
+                    
+                    break;
+                case 9:
+                    System.out.println("Opção 9");
+                    
+                    break;
+                default:
+                    System.out.println("Opção Inválida!");
+                    break;
+            }
+        } while (opcao != 9);
     }
 }
