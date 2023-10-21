e67cff4c72661c015f4dd81a1f960943be4b7239
CarlosEduardo <143044711+Caduzin3@users.noreply.github.com>
2023-10-21 19:01:15 -0400

    aluno

--- AULA-11/Atividade/Atividade - 06/FakeDB-Escola/src/com.uniderp.projetoescola/models/Aluno.java
+++ AULA-11/Atividade/Atividade - 06/FakeDB-Escola/src/com.uniderp.projetoescola/models/Aluno.java
@@ -9,47 +9,57 @@ public class Aluno {
     private double notaA2;
     private double notaP2;
     private double media;
+    private void atualizaMedia(){
+        this.media = (((notaA1+notaP1)/2) + ((notaA2+notaP2)/2))/2;
+    }
     public int getCodigo() {
         return codigo;
     }
     public void setCodigo(int codigo) {
         this.codigo = codigo;
+        this.atualizaMedia();
     }
     public String getNome() {
         return nome;
     }
     public void setNome(String nome) {
         this.nome = nome;
+        this.atualizaMedia();
     }
     public double getNotaA1() {
         return notaA1;
     }
     public void setNotaA1(double notaA1) {
         this.notaA1 = notaA1;
+        this.atualizaMedia();
     }
     public double getNotaP1() {
         return notaP1;
     }
     public void setNotaP1(double notaP1) {
         this.notaP1 = notaP1;
+        this.atualizaMedia();
     }
     public double getNotaA2() {
         return notaA2;
     }
     public void setNotaA2(double notaA2) {
         this.notaA2 = notaA2;
+        this.atualizaMedia();
     }
     public double getNotaP2() {
         return notaP2;
     }
     public void setNotaP2(double notaP2) {
         this.notaP2 = notaP2;
+        this.atualizaMedia();
     }
     public double getMedia() {
         return media;
     }
     public void setMedia(double media) {
         this.media = media;
+        this.atualizaMedia();
     }
     public Aluno() {
     }
@@ -57,9 +67,9 @@ public class Aluno {
         this.codigo = codigo;
         this.nome = nome;
     }
-
-    private void atualizaMedia(){
-        this.media = (((notaA1+notaP1)/2) + ((notaA2+notaP2)/2))/2;
+    @Override
+    public String toString() {
+        return "|codigo = " + codigo + " | nome = " + nome + " |";
     }
     
     
