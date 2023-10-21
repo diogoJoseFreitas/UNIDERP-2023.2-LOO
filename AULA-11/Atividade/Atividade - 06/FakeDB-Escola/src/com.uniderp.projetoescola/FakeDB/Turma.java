0c158b9987cafdcf564e6c0eed1ba3ff3c33deb6
Diogo Oliveira <djfreitasoliveira@gmail.com>
2023-10-21 18:35:42 -0400

    Classe Turma

--- AULA-11/Atividade/Atividade - 06/FakeDB-Escola/src/com.uniderp.projetoescola/FakeDB/Turma.java
+++ AULA-11/Atividade/Atividade - 06/FakeDB-Escola/src/com.uniderp.projetoescola/FakeDB/Turma.java
@@ -1,5 +1,25 @@
 package com.uniderp.projetoescola.FakeDB;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.uniderp.projetoescola.models.Aluno;
+
 public class Turma {
-    
+    private Scanner sc;
+    private ArrayList<Aluno> alunos;
+    public Turma(){
+        alunos = new ArrayList<Aluno>();
+        sc = new Scanner(System.in);
+    }
+
+    private void insereAluno(){
+        int codigo;
+        String nome;
+        System.out.println("Informe o código do Aluno: ");
+        codigo = sc.nextInt();
+        System.out.println("Informe o Nome do Aluno: ");
+        nome = sc.nextLine();
+        this.alunos.add(new Aluno(codigo, nome));
+    }
 }
